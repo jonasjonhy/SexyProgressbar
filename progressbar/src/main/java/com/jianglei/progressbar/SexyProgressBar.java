1e2bc2182f47da80ec892ecce5582cff7af380f4
longyi <longyi@2dfire.com>
2017-01-13 16:18:16 +0800

    暂存

--- progressbar/src/main/java/com/jianglei/progressbar/SexyProgressBar.java
+++ progressbar/src/main/java/com/jianglei/progressbar/SexyProgressBar.java
@@ -2,6 +2,7 @@ package com.jianglei.progressbar;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -10,6 +11,38 @@ import android.view.View;
  */
 
 public class SexyProgressBar extends View {
+
+    /**
+     * 已完成部分的进度条高度
+     */
+    private int completeBarHeight = 20;
+
+    /**
+     * 准备完成的进度条的高度
+     */
+    private int prepareBarHeight = 10;
+
+    private int width = 200;
+    /**
+     * 进度条达到的最大值，默认为100
+     */
+    private int maxValue=100;
+    /**
+     * 已经进行的进度
+     */
+    private int progress;
+
+    /**
+     * 进度条颜色
+     */
+    private int colorRes;
+    /**
+     * 画笔
+     */
+    private Paint paint;
+
+
+
     public SexyProgressBar(Context context) {
         super(context);
     }
@@ -22,8 +55,24 @@ public class SexyProgressBar extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    private void init(){
+        paint = new Paint();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        //绘制已经完成的进度条
+        if(progress != 0) {
+        }
+        //绘制数字
+        //绘制右侧待加载的进度条
+
+
     }
 }
