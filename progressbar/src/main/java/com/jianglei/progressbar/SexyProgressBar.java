a6dbfde15b142ac2778ec1442a7c9f36d2eb3e6e
longyi <longyi@2dfire.com>
2017-01-13 14:44:47 +0800

    init

---
+++ progressbar/src/main/java/com/jianglei/progressbar/SexyProgressBar.java
@@ -0,0 +1,29 @@
+package com.jianglei.progressbar;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by 龙衣 on 17-1-12.
+ */
+
+public class SexyProgressBar extends View {
+    public SexyProgressBar(Context context) {
+        super(context);
+    }
+
+    public SexyProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SexyProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
